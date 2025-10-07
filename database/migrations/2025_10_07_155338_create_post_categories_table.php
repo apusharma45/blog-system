d2a7117305af7975aa8a27b15ba4a12891c19ff5
apusharma45 <arapusharma45@gmail.com>
2025-10-07 21:59:09 +0600

    added necessary database tables

---
+++ database/migrations/2025_10_07_155338_create_post_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_categories', function (Blueprint $table) {
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+
+            // primary key
+            $table->primary(['post_id', 'category_id']);
+
+            // indexes
+            $table->index('post_id');
+            $table->index('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_categories');
+    }
+};
